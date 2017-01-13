dec004cc0a5b6de9c19af247bba3ee0d42adc878
kanadeko <kanaadeko@gmail.com>
2017-01-13 18:36:54 -0300

    Gallery WIP

--- controllers/galleryController.js
+++ controllers/galleryController.js
@@ -4,10 +4,25 @@ const db = require('knex')(config.database)
 let galleryController = {}
 
 galleryController.list = function(req, res, next){
-	if(!config.privacy.public)
-		if(!config.privacy.IPs.includes(req.ip)) return res.status(401).send('Not Authorized!')
+	//if(!config.privacy.public)
+		//if(!config.privacy.IPs.includes(req.ip)) return res.status(401).send('Not Authorized!')
 
 	db.table('gallery').select('id', 'name').then((data) => {
 		res.json({ data })
 	})
+}
+
+galleryController.test = function(req, res, next){
+	if(!config.privacy.public)
+		if(!config.privacy.IPs.includes(req.ip)) return res.status(401).send('Not Authorized!')
+
+	let testdata = [
+		{name: 'Test 1'},
+		{name: 'Test 2'},
+		{name: 'Test 3'},
+		{name: 'Test 4'},
+		{name: 'Test 5'}
+	]
+
+	db.table('gallery').insert(testdata).then(() => {})
 }
--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ const galleryController      = require('./controllers/galleryController')
 
 routes.post ('/upload', (req, res, next) => uploadController.upload(req, res, next))
 routes.get  ('/gallery', (req, res, next) => galleryController.list(req, res, next))
+routes.get  ('/gallery/test', (req, res, next) => galleryController.test(req, res, next))
 
 module.exports = routes
