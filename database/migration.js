c5b1e266713740ffa9fa9d4c279aa25e71000216
Bobby Wibowo <bobbywibowo98@gmail.com>
2018-03-14 14:12:12 +0700

    Updates
    * Adds 'enabled' column into 'users' table with database/migration.js file.
    
    * Ignore errors when adding new columns in database/migration.js. There's a better method by checking whether the columns already exist before adding them, but this will do for now.

--- database/migration.js
+++ database/migration.js
@@ -6,7 +6,10 @@ migration.start = async () => {
   await db.schema.table('albums', table => {
     table.dateTime('editedAt')
     table.dateTime('zipGeneratedAt')
-  })
+  }).catch(() => {})
+  await db.schema.table('users', table => {
+    table.integer('enabled')
+  }).catch(() => {})
   console.log('Migration finished! Now start lolisafe normally')
   process.exit(0)
 }
