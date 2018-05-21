df6d5459e038ef8edfb480e1d09cac25fa2c3ac6
Shumatsu <Shumatsu@users.noreply.github.com>
2018-05-21 23:13:39 +0200

    File extension bans
    `.nt` - turns out there's more Windows batch file extensions.
    `.psm1` - PowerScript extension.
    `.bash`, `.bsh`, `.csh`, `.bash_profile`, `.bashrc`, `.profile` - apparently those are valid bash script extensions.
    `.reg` - overwrites Windows registry keys upon execution.

--- config.sample.js
+++ config.sample.js
@@ -41,9 +41,18 @@ module.exports = {
 		'.com',
 		'.bat',
 		'.cmd',
+		'.nt',
 		'.scr',
 		'.ps1',
-		'.sh'
+		'.psm1',
+		'.sh',
+		'.bash',
+		'.bsh',
+		'.csh',
+		'.bash_profile',
+		'.bashrc',
+		'.profile',
+		'.reg'
 	],
 
 	// Uploads config
