7df56eb91c4cf22c6e7323e24881bc527a2c1ad6
Pitu <7425261+Pitu@users.noreply.github.com>
2018-09-16 17:53:14 -0300

    Switching to postgresql as the default had some implications

--- src/api/routes/files/uploadPOST.js
+++ src/api/routes/files/uploadPOST.js
@@ -137,7 +137,11 @@ class uploadPOST extends Route {
 				ip: req.ip,
 				createdAt: now,
 				editedAt: now
-			});
+			}, 'id');
+			/*
+				TODO: Something funny here, I'm not sure since I don't use MySQL but I think the argument id
+				on the insert function on top behaves differently on psql/mysql/sqlite. Needs testing.
+			*/
 		} catch (error) {
 			log.error('There was an error saving the file to the database');
 			log.error(error);
