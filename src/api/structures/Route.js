25c5a06ec3e363f5b98607949b76b8b395e4c962
Pitu <heyitspitu@gmail.com>
2019-02-21 23:05:56 +0900

    derp

--- src/api/structures/Route.js
+++ src/api/structures/Route.js
@@ -4,7 +4,7 @@ const db = require('knex')({
 	connection: {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
+		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE
 	}
 });
--- src/api/utils/Util.js
+++ src/api/utils/Util.js
@@ -8,7 +8,7 @@ const db = require('knex')({
 	connection: {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
+		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE
 	}
 });
