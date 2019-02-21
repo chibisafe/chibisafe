c7a4a39de4e6113e88f07fefb3668e9fd3b1372a
Pitu <heyitspitu@gmail.com>
2019-02-22 00:00:07 +0900

    Add support for sqlite

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE
+		database: process.env.DB_DATABASE,
+		filename: 'database.sqlite'
 	},
 	pool: {
 		min: process.env.DATABASE_POOL_MIN || 2,
--- src/api/utils/Util.js
+++ src/api/utils/Util.js
@@ -9,7 +9,8 @@ const db = require('knex')({
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE
+		database: process.env.DB_DATABASE,
+		filename: '../../../database.sqlite'
 	}
 });
 const moment = require('moment');
