d7745aff40104c8b7104f6a56ff8d7a9186dc10d
Pitu <heyitspitu@gmail.com>
2019-03-15 07:34:19 +0000

    Turns out we were using the wrong cookieparser

--- src/site/store/index.js
+++ src/site/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-const cookieparser = process.server ? require('cookie-parser') : null;
+const cookieparser = process.server ? require('cookieparser') : null;
 
 export const state = () => ({
 	loggedIn: false,
