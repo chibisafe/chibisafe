b27b4c47f79071d3aa336b8f9a6578103df56fe9
Pitu <heyitspitu@gmail.com>
2020-05-10 21:22:25 +0900

    feat: Proper deleting of albums

--- src/api/routes/albums/albumDELETE.js
+++ src/api/routes/albums/albumDELETE.js
@@ -17,10 +17,18 @@ class albumDELETE extends Route {
 		if (!album) return res.status(400).json({ message: 'The album doesn\'t exist or doesn\'t belong to the user' });
 
 		try {
-			/*
-				Delete the album
-			*/
+			// Delete the album
 			await db.table('albums').where({ id }).delete();
+
+			// Delete the relation of any files attached to this album
+			await db.table('albumsFiles').where({ albumId: id }).delete();
+
+			// Delete the relation of any links attached to this album
+			await db.table('albumsLinks').where({ albumId: id }).delete();
+
+			// Delete any album links created for this album
+			await db.table('links').where({ albumId: id }).delete();
+
 			return res.json({ message: 'The album was deleted successfully' });
 		} catch (error) {
 			return super.error(res, error);
