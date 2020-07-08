704578e964741da85b29eeb32cb7e447c3a4c19b
Zephyrrus <krisztian1997@gmail.com>
2020-07-09 02:22:48 +0300

    feat: add handler for actions
    Takes care of dispatching a 🍞 on success/error

---
+++ src/site/plugins/handler.js
@@ -0,0 +1,25 @@
+import AlertTypes from '~/constants/alertTypes';
+
+export default ({ store }, inject) => {
+	inject('handler', {
+		async executeAction(action, param) {
+			try {
+				const response = await store.dispatch(action, param);
+
+				store.commit('alert/set', {
+					message: response?.message ?? 'Executed sucesfully',
+					type: AlertTypes.SUCCESS,
+				});
+
+				return response;
+			} catch (e) {
+				store.commit('alert/set', {
+					message: e.message,
+					type: AlertTypes.ERROR,
+				});
+
+				return null;
+			}
+		},
+	});
+};
