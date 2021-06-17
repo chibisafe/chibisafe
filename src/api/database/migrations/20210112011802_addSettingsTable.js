acc7da5310569846d2d2514dc065cc903499b9b3
Pitu <heyitspitu@gmail.com>
2021-06-18 02:53:52 +0900

    test: seeding during migration

--- src/api/database/migrations/20210112011802_addSettingsTable.js
+++ src/api/database/migrations/20210112011802_addSettingsTable.js
@@ -1,8 +1,23 @@
+const Util = require('../../utils/Util');
+
 exports.up = async knex => {
 	await knex.schema.createTable('settings', table => {
 		table.string('key');
 		table.string('value');
 	});
+
+	try {
+		const defaults = Util.getEnvironmentDefaults();
+		const keys = Object.keys(defaults);
+		for (const item of keys) {
+			await Util.writeConfigToDb({
+				key: item,
+				value: defaults[item]
+			});
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 exports.down = async knex => {
