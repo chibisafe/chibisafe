581c9b8a984b4b2b721e4d5247b1661d4d50831a
Pitu <heyitspitu@gmail.com>
2022-09-18 18:38:08 +0900

    fix: auth middleware

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -1,4 +1,4 @@
-import type { Response } from 'hyper-express';
+import type { Response, MiddlewareNext } from 'hyper-express';
 import type { RequestWithUser } from '../structures/interfaces';
 import JWT from 'jsonwebtoken';
 import prisma from '../structures/database';
@@ -7,14 +7,14 @@ interface Decoded {
 	sub: number;
 }
 
-export default (req: RequestWithUser, res: Response) => {
+export default (req: RequestWithUser, res: Response, next: MiddlewareNext) => {
 	if (!req.headers.authorization) return res.status(401).json({ message: 'No authorization header provided' });
 
 	const token = req.headers.authorization.split(' ')[1];
 	if (!token) return res.status(401).json({ message: 'No authorization header provided' });
 
 	// eslint-disable-next-line @typescript-eslint/no-misused-promises
-	JWT.verify(token, process.env.secret ?? '', async (error, decoded) => {
+	JWT.verify(token, process.env.JWT_SECRET ?? '', async (error, decoded) => {
 		if (error) return res.status(401).json({ message: 'Invalid token' });
 		const id = (decoded as Decoded | undefined)?.sub ?? null;
 		if (!id) return res.status(401).json({ message: 'Invalid authorization' });
@@ -33,5 +33,6 @@ export default (req: RequestWithUser, res: Response) => {
 
 		if (!user) return res.status(401).json({ message: "User doesn't exist" });
 		req.user = user;
+		next();
 	});
 };
