0ed695874a2a26850020f10d0b048c1a51e4bba9
Bobby <bobby@fiery.me>
2022-09-27 02:15:13 +0700

    chore: always empty chunks dir on start

--- src/api/main.ts
+++ src/api/main.ts
@@ -40,7 +40,7 @@ const start = async () => {
 	);
 
 	// Create the neccessary folders
-	jetpack.dir('uploads/chunks');
+	jetpack.dir('uploads/chunks', { empty: true });
 	jetpack.dir('uploads/thumbs/square');
 	jetpack.dir('uploads/thumbs/preview');
 
