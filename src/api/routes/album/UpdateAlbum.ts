aa94367931f6fb5e2d9eced0122f54a3474dd4fa
Pitu <heyitspitu@gmail.com>
2022-10-03 19:40:56 +0900

    feat: UpdateAlbum

---
+++ src/api/routes/album/UpdateAlbum.ts
@@ -0,0 +1,45 @@
+import type { Response } from 'hyper-express';
+import prisma from '../../structures/database';
+import { RequestWithUser } from '../../structures/interfaces';
+
+export const options = {
+	url: '/album/:uuid/update',
+	method: 'post',
+	middlewares: ['auth']
+};
+
+export const run = async (req: RequestWithUser, res: Response) => {
+	const { uuid } = req.path_parameters;
+	if (!uuid) return res.status(400).json({ message: 'Invalid uuid supplied' });
+
+	const { name, nsfw } = await req.json();
+	if (!name && nsfw === undefined) return res.status(400).json({ message: 'No data supplied' });
+
+	// Make sure the album exists and belongs to the user
+	const album = await prisma.albums.findFirst({
+		where: {
+			uuid,
+			userId: req.user.id
+		}
+	});
+
+	if (!album) return res.status(401).json({ message: "The album doesn't exist or doesn't belong to the user" });
+
+	const updateObj = {
+		name: name || album.name,
+		nsfw: nsfw === true ? true : nsfw === false ? false : album.nsfw
+	};
+
+	await prisma.albums.update({
+		where: {
+			uuid
+		},
+		data: {
+			...updateObj
+		}
+	});
+
+	return res.json({
+		message: 'Successfully edited the album'
+	});
+};
