8579e17ea69a68bfdbba9e2bbe3859349f671c96
Pitu <heyitspitu@gmail.com>
2022-10-03 20:57:19 +0900

    wip: ZipAlbum

---
+++ src/api/routes/album/_ZipAlbum.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from 'hyper-express';
+
+export const options = {
+	// url: '/album/:identifier/zip',
+	method: 'get'
+};
+
+export const run = async (req: Request, res: Response) => {};
