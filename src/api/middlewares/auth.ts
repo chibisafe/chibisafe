bef02f60708c8f29e4637cf6c60e44351d2ad623
Pitu <heyitspitu@gmail.com>
2022-10-03 20:59:50 +0900

    wip: figure out why sometimes it takes so long for a request to go through

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -14,23 +14,30 @@ export default (
 	next: MiddlewareNext,
 	options?: { [index: string | number]: any }
 ) => {
+	console.time('authorization');
 	if (!req.headers.authorization) {
 		if (options?.optional) return next();
 		return res.status(401).json({ message: 'No authorization header provided' });
 	}
+	console.timeEnd('authorization');
 
+	console.time('token');
 	const token = req.headers.authorization.split(' ')[1];
 	if (!token) {
 		if (options?.optional) return next();
 		return res.status(401).json({ message: 'No authorization header provided' });
 	}
+	console.timeEnd('token');
 
+	console.time('jwt');
 	// eslint-disable-next-line @typescript-eslint/no-misused-promises
 	JWT.verify(token, process.env.JWT_SECRET ?? '', async (error, decoded) => {
+		console.timeEnd('jwt');
 		if (error) return res.status(401).json({ message: 'Invalid token' });
 		const id = (decoded as Decoded | undefined)?.sub ?? null;
 		if (!id) return res.status(401).json({ message: 'Invalid authorization' });
 
+		console.time('user');
 		const user = await prisma.users.findFirst({
 			where: {
 				id
@@ -42,6 +49,7 @@ export default (
 				isAdmin: true
 			}
 		});
+		console.timeEnd('user');
 
 		if (!user) return res.status(401).json({ message: "User doesn't exist" });
 		req.user = user;
--- src/api/routes/files/GetFile.ts
+++ src/api/routes/files/GetFile.ts
@@ -14,15 +14,18 @@ export const run = async (req: RequestWithUser, res: Response) => {
 	if (!uuid) return res.status(400).json({ message: 'Invalid uuid supplied' });
 
 	// Make sure the file exists and belongs to the user
+	console.time('file');
 	const file = await prisma.files.findFirst({
 		where: {
 			uuid,
 			userId: req.user.id
 		}
 	});
+	console.timeEnd('file');
 
 	if (!file) return res.status(404).json({ message: 'The file could not be found' });
 
+	console.time('albumsFiles');
 	// TODO: Figure out how to join with prisma, probably need to update the schema for it
 	const albumsFiles = await prisma.albumsFiles.findMany({
 		where: {
@@ -32,7 +35,9 @@ export const run = async (req: RequestWithUser, res: Response) => {
 			albumId: true
 		}
 	});
+	console.timeEnd('albumsFiles');
 
+	console.time('albums');
 	// Grab all the albums the file is part of
 	const albums = await prisma.albums.findMany({
 		where: {
@@ -48,7 +53,9 @@ export const run = async (req: RequestWithUser, res: Response) => {
 			id: 'desc'
 		}
 	});
+	console.timeEnd('albums');
 
+	console.time('fileTags');
 	// Grab all the tags the file is part of
 	const fileTags = await prisma.fileTags.findMany({
 		where: {
@@ -58,7 +65,9 @@ export const run = async (req: RequestWithUser, res: Response) => {
 			tagId: true
 		}
 	});
+	console.timeEnd('fileTags');
 
+	console.time('tags');
 	const tags = await prisma.tags.findMany({
 		where: {
 			id: {
@@ -73,10 +82,13 @@ export const run = async (req: RequestWithUser, res: Response) => {
 			id: 'desc'
 		}
 	});
+	console.timeEnd('tags');
 
+	console.time('parsedFile');
 	// Build the public links
 	let parsedFile: ExtendedFile = file;
 	parsedFile = constructFilePublicLink(req, file);
+	console.timeEnd('parsedFile');
 
 	return res.json({
 		message: 'Successfully retrieved file',
