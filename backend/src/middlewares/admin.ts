18732a3170761f249ecb1e553f00dbd4fce8b978
Bobby <bobby@fiery.me>
2022-10-04 21:10:39 +0700

    fix: admin middleware

--- backend/src/middlewares/admin.ts
+++ backend/src/middlewares/admin.ts
@@ -1,6 +1,10 @@
-import type { Response } from 'hyper-express';
+import type { Response, MiddlewareNext } from 'hyper-express';
 import type { RequestWithUser } from '../structures/interfaces';
 
-export default (req: RequestWithUser, res: Response) => {
-	if (!req.user.isAdmin) return res.status(401).json({ message: 'Permission denied' });
+export default (req: RequestWithUser, res: Response, next: MiddlewareNext) => {
+	if (!req.user?.isAdmin) {
+		return res.status(401).json({ message: 'Permission denied' });
+	}
+
+	next();
 };
