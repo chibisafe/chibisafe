fda93b87efd27157565a84cbf73d75146dc366ac
Pitu <heyitspitu@gmail.com>
2022-10-04 23:18:32 +0900

    feat: serve frontend when running in production mode

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,10 @@
 import HyperExpress from 'hyper-express';
+// @ts-ignore
+import LiveDirectory from 'live-directory';
 import jetpack from 'fs-jetpack';
 import log from './utils/Log';
 import process from 'node:process';
+import path from 'node:path';
 // import helmet from 'helmet';
 import cors from 'cors';
 import Routes from './structures/routes';
@@ -64,11 +67,27 @@ const start = async () => {
 	// Scan and load routes into express
 	await Routes.load(server);
 
-	// TODO: Uploader page
-	server.get('/uploader.html', (req, res) => {
-		const readStream = jetpack.createReadStream('src/site/uploader.html');
-		res.stream(readStream);
-	});
+	if (process.env.NODE_ENV === 'production') {
+		if (!jetpack.exists(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'))) {
+			log.error('Frontend build not found, please run `npm run build` in the frontend directory');
+			process.exit(1);
+		}
+
+		const LiveAssets = new LiveDirectory({
+			path: path.join(__dirname, '..', '..', 'frontend', 'dist')
+		});
+
+		server.get('/', (req, res) => {
+			const file = LiveAssets.get('index.html');
+			return res.type(file.extension).send(file.buffer);
+		});
+
+		server.get('/*', (req, res) => {
+			const file = LiveAssets.get(req.path);
+			if (!file) return res.status(404).send('Not found');
+			return res.type(file.extension).send(file.buffer);
+		});
+	}
 
 	// Start the server
 	await server.listen(8000);
