71af3b10b98395cb676a2999c6bbadb1cbaf501e
Pitu <heyitspitu@gmail.com>
2022-10-04 22:50:59 +0900

    chore: re-arrange shared files between projects

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import { createMetaManager } from 'vue-meta';
 
 // @ts-ignore
 import App from './App.vue';
-import router from './router.ts';
+import router from './router';
 
 const mount = async () => {
 	const app = createApp(App).use(createPinia()).use(router).use(createMetaManager());
