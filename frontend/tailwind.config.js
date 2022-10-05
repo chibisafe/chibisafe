5afacb934312ab49ff638a92984ae03f283cc427
Pitu <heyitspitu@gmail.com>
2022-10-06 00:28:22 +0900

    chore: font change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans]
+				sans: ['Nunito', ...defaultTheme.fontFamily.sans]
 			},
 			colors: {
 				chibisafe: {
