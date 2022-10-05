9017647b28edf8edc44b4e3352218cb5bfce45c8
Pitu <heyitspitu@gmail.com>
2022-10-06 00:06:08 +0900

    feat: first approach at a landing page

---
+++ frontend/src/components/about/About.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="absolute bottom-4 left-4 dark:text-chibisafe-text-light">
+		<p class="font-semibold text-xl text-center">
+			<a
+				href="https://github.com/WeebDev/chibisafe"
+				rel="noopener noreferrer"
+				target="_blank"
+				class="hover:text-blue-400"
+				>chibisafe <span class="text-sm">v6.0.0</span></a
+			>
+		</p>
+	</div>
+</template>
---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex w-full h-12 items-center mt-4">
+		<router-link to="/">
+			<img src="../../assets/images/logo.png" alt="chibisafe logo" class="w-12 mr-2" />
+		</router-link>
+		<router-link to="/">
+			<span class="font-bold text-3xl">Chibisafe</span>
+		</router-link>
+		<span class="flex-1" />
+		<span>
+			<a
+				href="https://github.com/WeebDev/chibisafe"
+				rel="noopener noreferrer"
+				target="_blank"
+				class="hover:text-blue-400"
+				><IconGitHub class="w-8 h-8"
+			/></a>
+		</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+// @ts-ignore
+import IconGitHub from '~icons/carbon/logo-github';
+</script>
