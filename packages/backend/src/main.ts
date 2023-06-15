6397f33ac0ba68e83956b30082170c023587b8a5
Kana <heyitspitu@gmail.com>
2023-06-15 01:27:24 -0300

    chore: stuff

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -223,6 +223,8 @@ const start = async () => {
 
 	// Start the server
 	await server.listen({ port: Number(SETTINGS.port), host: SETTINGS.host as string });
+	if (process.env.NODE_ENV === 'production')
+		console.log(`Chibisafe is listening on ${SETTINGS.host}:${SETTINGS.port}`);
 	// Jumpstart statistics scheduler
 	await jumpstartStatistics();
 };
