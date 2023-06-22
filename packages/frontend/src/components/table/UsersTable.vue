af78178e6c1bfc9a4eef84221f3d8dc6a3598871
Kana <heyitspitu@gmail.com>
2023-06-22 00:03:41 -0300

    [Fix] Tables design (#412)

--- packages/frontend/src/components/table/UsersTable.vue
+++ packages/frontend/src/components/table/UsersTable.vue
@@ -4,43 +4,43 @@
 			<tr>
 				<th
 					scope="col"
-					class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:pl-6"
+					class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:pl-6"
 				>
 					Username
 				</th>
 				<th
 					scope="col"
-					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				>
 					Files
 				</th>
 				<th
 					scope="col"
-					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				>
 					Status
 				</th>
 				<th
 					scope="col"
-					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				>
 					Role
 				</th>
 				<th
 					scope="col"
-					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				>
 					Space used
 				</th>
 				<th
 					scope="col"
-					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="hidden px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				>
 					Created
 				</th>
 				<th
 					scope="col"
-					class="px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 sm:table-cell"
+					class="px-3 py-3.5 text-left text-sm font-semibold text-dark-90 dark:text-light-100 desktop:table-cell"
 				></th>
 			</tr>
 		</thead>
@@ -51,26 +51,26 @@
 				:class="indexUser % 2 === 0 ? ' bg-dark-90' : 'bg-dark-80'"
 			>
 				<td
-					class="w-full px-3 max-w-0 py-4 pl-4 pr-3 font-normal text-dark-90 dark:text-light-100 sm:w-auto sm:max-w-none sm:pl-6 underline"
+					class="w-full px-3 max-w-0 py-4 pl-4 pr-3 font-normal text-dark-90 dark:text-light-100 desktop:w-auto desktop:max-w-none desktop:pl-6 underline"
 				>
 					<router-link :to="`/dashboard/admin/user/${user.uuid}`">{{ user.username }}</router-link>
 				</td>
-				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 sm:table-cell">
+				<td class="px-3 py-4 text-sm text-dark-90 dark:text-light-100 desktop:table-cell">
 					{{ user._count.files }}
 				</td>
-				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 sm:table-cell">
+				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 desktop:table-cell">
 					{{ user.enabled ? 'Enabled' : 'Disabled' }}
 				</td>
-				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 sm:table-cell">
+				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 desktop:table-cell">
 					{{ user.isAdmin ? 'Admin' : 'User' }}
 				</td>
-				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 sm:table-cell">
+				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 desktop:table-cell">
 					{{ formatBytes(Number(user.size)) }}
 				</td>
-				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 sm:table-cell">
+				<td class="hidden px-3 py-4 text-sm text-dark-90 dark:text-light-100 desktop:table-cell">
 					{{ dayjs(user.createdAt).format('MMMM D, YYYY h:mm A') }}
 				</td>
-				<td class="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 text-dark-90 dark:text-light-100">
+				<td class="py-4 pl-3 pr-4 text-right text-sm font-medium desktop:pr-6 text-dark-90 dark:text-light-100">
 					<router-link :to="`/dashboard/admin/user/${user.uuid}`" class="underline">View files</router-link>
 					<!-- Only show one action if we're the user on the table -->
 					<template v-if="user.uuid === ownUser.uuid">
