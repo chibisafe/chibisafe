6a6aebcc0eff64c8d6c1c8835cb9ae3b97e6aae0
Kana <heyitspitu@gmail.com>
2023-06-22 18:48:03 -0300

    fix: styling issues

--- packages/frontend/src/components/modals/ManageUserModal.vue
+++ packages/frontend/src/components/modals/ManageUserModal.vue
@@ -14,20 +14,20 @@
 			</TransitionChild>
 
 			<div class="fixed inset-0 z-10 overflow-y-auto">
-				<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+				<div class="flex min-h-full items-end justify-center p-4 text-center desktop:items-center desktop:p-0">
 					<TransitionChild
 						as="template"
 						enter="ease-out duration-300"
-						enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-						enter-to="opacity-100 translate-y-0 sm:scale-100"
+						enter-from="opacity-0 translate-y-4 desktop:translate-y-0 desktop:scale-95"
+						enter-to="opacity-100 translate-y-0 desktop:scale-100"
 						leave="ease-in duration-200"
-						leave-from="opacity-100 translate-y-0 sm:scale-100"
-						leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+						leave-from="opacity-100 translate-y-0 desktop:scale-100"
+						leave-to="opacity-0 translate-y-4 desktop:translate-y-0 desktop:scale-95"
 					>
 						<DialogPanel
-							class="relative transform overflow-hidden rounded-lg bg-dark-110 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+							class="relative transform overflow-hidden rounded-lg bg-dark-110 px-4 pt-5 pb-4 text-left shadow-xl transition-all desktop:my-8 desktop:w-full desktop:max-w-lg desktop:p-6"
 						>
-							<div class="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
+							<div class="absolute top-0 right-0 hidden pt-4 pr-4 desktop:block">
 								<button
 									type="button"
 									class="rounded-md bg-dark-110 text-light-100 hover:text-gray-500 focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:ring-offset-2"
@@ -37,8 +37,8 @@
 									<IconClose class="h-6 w-6" aria-hidden="true" />
 								</button>
 							</div>
-							<div class="sm:flex sm:items-start">
-								<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+							<div class="desktop:flex desktop:items-start">
+								<div class="mt-3 text-center desktop:mt-0 desktop:ml-4 desktop:text-left">
 									<DialogTitle as="h3" class="text-lg font-medium leading-6 text-light-100">
 										<span class="capitalize">{{ action }} user </span>
 										<span class="font-bold">{{ user?.username }}</span>
@@ -68,17 +68,17 @@
 									</div>
 								</div>
 							</div>
-							<div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+							<div class="mt-5 desktop:mt-4 desktop:flex desktop:flex-row-reverse">
 								<button
 									type="button"
-									class="inline-flex w-full justify-center rounded-md bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-400 focus:outline-none focus:ring-0 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+									class="inline-flex w-full justify-center rounded-md bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-400 focus:outline-none focus:ring-0 focus:ring-red-500 focus:ring-offset-2 desktop:ml-3 desktop:w-auto desktop:text-sm"
 									@click="doAction"
 								>
 									<span class="capitalize">{{ action }}</span>
 								</button>
 								<button
 									type="button"
-									class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+									class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:ring-offset-2 desktop:mt-0 desktop:w-auto desktop:text-sm"
 									@click="closeModal"
 								>
 									Cancel
