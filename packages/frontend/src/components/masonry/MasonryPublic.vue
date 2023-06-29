f61c6c17df4478edf9b34d0805221d6a840b6725
Kana <heyitspitu@gmail.com>
2023-06-29 03:21:41 -0300

    feat: make masonry prettier

--- packages/frontend/src/components/masonry/MasonryPublic.vue
+++ packages/frontend/src/components/masonry/MasonryPublic.vue
@@ -1,33 +1,35 @@
 <template>
 	<div v-if="files?.length" class="masonry mt-8 pb-16">
-		<div v-for="file in files" :key="file.uuid">
-			<a :href="file.url" target="_blank" rel="noopener noreferrer">
-				<template v-if="isFileImage(file) || isFileVideo(file)">
-					<img
-						v-element-hover="(value: boolean) => onHover(value, file.uuid)"
-						:src="file.thumb"
-						class="cursor-pointer w-full"
-					/>
+		<div v-for="file in files" :key="file.uuid" class="bg-black">
+			<div class="hover:-translate-y-1 hover:translate-x-1 transition-transform duration-100 ease-in-out">
+				<a :href="file.url" target="_blank" rel="noopener noreferrer">
+					<template v-if="isFileImage(file) || isFileVideo(file)">
+						<img
+							v-element-hover="(value: boolean) => onHover(value, file.uuid)"
+							:src="file.thumb"
+							class="cursor-pointer w-full"
+						/>
 
-					<video
-						v-if="isHovered[file.uuid]"
-						class="preview absolute top-0 left-0 w-full h-full pointer-events-none"
-						autoplay
-						loop
-						muted
-					>
-						<source :src="file.preview" type="video/mp4" />
-					</video>
+						<video
+							v-if="isHovered[file.uuid]"
+							class="preview absolute top-0 left-0 w-full h-full pointer-events-none"
+							autoplay
+							loop
+							muted
+						>
+							<source :src="file.preview" type="video/mp4" />
+						</video>
 
-					<VideoIcon v-if="isFileVideo(file)" class="absolute bottom-1 right-1 w-6 h-6 text-light-100" />
-				</template>
-				<div v-else class="w-full h-40 bg-dark-90 flex flex-col justify-center items-center cursor-pointer">
-					<FileAudioIcon v-if="isFileAudio(file)" class="text-dark-100 dark:text-light-100 w-16 h-16" />
-					<FileTextIcon v-else-if="isFilePDF(file)" class="text-dark-100 dark:text-light-100 w-16 h-16" />
-					<FileIcon v-else class="text-dark-100 dark:text-light-100 w-16 h-16" />
-					<span class="text-dark-100 dark:text-light-100 mt-4 text-lg">{{ file.original }}</span>
-				</div>
-			</a>
+						<VideoIcon v-if="isFileVideo(file)" class="absolute bottom-1 right-1 w-6 h-6 text-light-100" />
+					</template>
+					<div v-else class="w-full h-40 bg-dark-90 flex flex-col justify-center items-center cursor-pointer">
+						<FileAudioIcon v-if="isFileAudio(file)" class="text-dark-100 dark:text-light-100 w-16 h-16" />
+						<FileTextIcon v-else-if="isFilePDF(file)" class="text-dark-100 dark:text-light-100 w-16 h-16" />
+						<FileIcon v-else class="text-dark-100 dark:text-light-100 w-16 h-16" />
+						<span class="text-dark-100 dark:text-light-100 mt-4 text-lg">{{ file.original }}</span>
+					</div>
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
