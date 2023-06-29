abcee7cee5417297a09f3ec2a9a04a0fe8c45a02
Kana <heyitspitu@gmail.com>
2023-06-29 19:04:01 -0300

    feat: set quota route

---
+++ packages/backend/src/routes/admin/users/SetQuota.schema.ts
@@ -0,0 +1,35 @@
+export default {
+	summary: 'Promote user',
+	description: 'Promote a user to admin',
+	tags: ['User Management'],
+	params: {
+		type: 'object',
+		properties: {
+			uuid: {
+				type: 'string',
+				description: 'The uuid of the user to promote.'
+			}
+		},
+		required: ['uuid']
+	},
+	body: {
+		type: 'object',
+		properties: {
+			space: {
+				type: 'number',
+				description: 'The amount of space to allocate.'
+			}
+		},
+		required: ['space']
+	},
+	response: {
+		200: {
+			type: 'object',
+			properties: {
+				message: { $ref: 'ResponseMessage' }
+			}
+		},
+		'4xx': { $ref: 'HTTP4xxError' },
+		'5xx': { $ref: 'HTTP5xxError' }
+	}
+};
