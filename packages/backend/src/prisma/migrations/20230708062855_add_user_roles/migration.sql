7ae3fdc3b9ce2ae2d99ccda0f7b8cc6c37c29bdd
Kana <heyitspitu@gmail.com>
2023-07-08 16:16:02 -0300

    feat: drop isAdmin column after migration

--- packages/backend/src/prisma/migrations/20230708062855_add_user_roles/migration.sql
+++ packages/backend/src/prisma/migrations/20230708062855_add_user_roles/migration.sql
@@ -26,10 +26,33 @@ INSERT INTO "roles" ("name") VALUES ('owner');
 INSERT INTO "roles" ("name") VALUES ('admin');
 INSERT INTO "roles" ("name") VALUES ('user');
 INSERT INTO "_rolesTousers" ("B", "A") SELECT "id", (SELECT "id" FROM "roles" WHERE "name" = 'admin') FROM "users" WHERE "isAdmin" = true;
-INSERT INTO "_rolesTousers" ("B", "A") SELECT "id", (SELECT "id" FROM "roles" WHERE "name" = 'user') FROM "users" WHERE "isAdmin" = false;
+INSERT INTO "_rolesTousers" ("B", "A") SELECT "id", (SELECT "id" FROM "roles" WHERE "name" = 'user') FROM "users";
 
 -- Grab the first admin user that is not disabled
 -- and assign the owner role to it
 INSERT INTO "_rolesTousers" ("B", "A") SELECT "id", (SELECT "id" FROM "roles" WHERE "name" = 'owner') FROM "users" WHERE "isAdmin" = true AND "enabled" = true LIMIT 1;
 
 --- Delete the old isAdmin column from the users table
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uuid" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "storageQuota" TEXT NOT NULL DEFAULT '0',
+    "apiKey" TEXT,
+    "passwordEditedAt" DATETIME,
+    "apiKeyEditedAt" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "editedAt" DATETIME
+);
+INSERT INTO "new_users" ("apiKey", "apiKeyEditedAt", "createdAt", "editedAt", "enabled", "id", "password", "passwordEditedAt", "storageQuota", "username", "uuid") SELECT "apiKey", "apiKeyEditedAt", "createdAt", "editedAt", "enabled", "id", "password", "passwordEditedAt", "storageQuota", "username", "uuid" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_uuid_key" ON "users"("uuid");
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+CREATE UNIQUE INDEX "users_apiKey_key" ON "users"("apiKey");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
