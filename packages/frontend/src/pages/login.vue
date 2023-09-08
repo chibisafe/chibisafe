f40a3c7997e1bb6894935a1db02c4958e8972250
Kana <heyitspitu@gmail.com>
2023-09-09 04:39:00 +0900

    feat: make login and register pages slick

--- packages/frontend/src/pages/login.vue
+++ packages/frontend/src/pages/login.vue
@@ -1,51 +1,81 @@
 <template>
-	<div class="h-screen w-full overflow-auto">
-		<div class="flex flex-col items-center w-full self-center text-light-100">
-			<Header />
-		</div>
-		<div
-			class="flex flex-col items-center h-auto min-h-[900px] mobile:min-h-0 w-full text-light-100 justify-center self-center"
+	<div
+		class="container relative h-screen flex-col items-center justify-center grid max-w-none grid-cols-2 px-0 bg-dark-110 mobile:flex"
+	>
+		<router-link
+			to="/register"
+			class="text-blue-500"
+			:class="cn(buttonVariants({ variant: 'ghost' }), 'absolute mobile:right-4 mobile:top-4 right-8 top-8')"
 		>
-			<div class="flex w-full mt-16 mobile:mt-0 items-center relative max-w-4xl flex-col">
-				<div class="flex min-h-full flex-col justify-center py-12 mobile:py-0 desktop:px-6 lg:px-8 w-full">
-					<div class="desktop:mx-auto desktop:w-full desktop:max-w-md">
-						<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-light-100">
-							Authentication
-						</h2>
-						<p class="mt-4 text-center">
-							If you don't have an account yet please
-							<router-link to="/register" class="text-blue-500">click here</router-link>
-						</p>
+			Open an account
+		</router-link>
+		<div class="relative mobile:hidden h-full flex-col bg-muted p-10 text-white border-r flex">
+			<div
+				class="absolute inset-0 bg-no-repeat bg-scroll bg-center bg-cover"
+				:style="`background-image: url(${settingsStore.backgroundImageURL});`"
+			/>
+			<div class="relative z-20 flex items-center text-lg font-medium">
+				<router-link to="/">
+					<img
+						v-if="settingsStore.logoURL"
+						:src="settingsStore.logoURL"
+						alt="chibisafe logo"
+						class="w-12 mr-2"
+					/>
+					<img v-else src="/logo.svg" alt="chibisafe logo" class="w-12 mr-2" />
+				</router-link>
+				<router-link to="/">
+					<span class="font-bold text-3xl">{{ settingsStore.serviceName }}</span>
+				</router-link>
+			</div>
+			<div class="relative z-20 mt-auto">
+				<blockquote class="space-y-2">
+					<p class="text-lg">
+						<strong>chibisafe</strong> is a modern and self-hosted take on file uploading services that can
+						handle anything you throw at it thanks to it's robust and fast API, chunked uploads support and
+						more.
+					</p>
+				</blockquote>
+			</div>
+		</div>
+		<div class="p-8 relative">
+			<div class="mx-auto flex mobile:w-full flex-col justify-center space-y-6 w-[350px]">
+				<div class="flex flex-col space-y-2 text-center">
+					<h1 class="text-2xl font-semibold tracking-tight">Authentication</h1>
+					<p class="text-sm text-muted-foreground">Enter your credentials below to log in</p>
+				</div>
+				<div class="grid gap-2">
+					<div class="grid gap-1">
+						<Label class="sr-only" for="username"> Username </Label>
+						<Input
+							id="username"
+							v-model="username"
+							placeholder="Username"
+							type="text"
+							auto-capitalize="none"
+							auto-complete="username"
+							auto-correct="off"
+							:disabled="isLoading"
+						/>
 					</div>
-
-					<div class="mt-8 desktop:mx-auto desktop:w-full desktop:max-w-md">
-						<div class="bg-dark-110 py-8 px-4 shadow desktop:rounded-lg desktop:px-10">
-							<form class="space-y-6" action="#" method="POST">
-								<div>
-									<label class="block text-sm font-medium text-light-100">Username</label>
-									<div class="mt-1">
-										<Input v-model="username" type="text" autocomplete="username" />
-									</div>
-								</div>
-
-								<div>
-									<label class="block text-sm font-medium text-light-100">Password</label>
-									<div class="mt-1">
-										<Input
-											v-model="password"
-											type="password"
-											autocomplete="current-password"
-											@keyup.enter="login"
-										/>
-									</div>
-								</div>
-
-								<div class="mt-4 justify-end flex gap-1">
-									<Button variant="link" @click="login">Login</Button>
-								</div>
-							</form>
-						</div>
+					<div class="grid gap-1">
+						<Label class="sr-only" for="password"> Password </Label>
+						<Input
+							id="password"
+							v-model="password"
+							placeholder="Password"
+							type="password"
+							auto-capitalize="none"
+							auto-complete="password"
+							auto-correct="off"
+							:disabled="isLoading"
+							@keyup.enter="login"
+						/>
 					</div>
+					<Button :disabled="isLoading" @click="login">
+						<Loader2Icon v-if="isLoading" class="mr-2 h-4 w-4 animate-spin" />
+						Sign In
+					</Button>
 				</div>
 			</div>
 		</div>
@@ -55,14 +85,18 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
-import { useToastStore } from '~/store/toast';
-import { useUserStore } from '~/store/user';
-import Button from '~/components/buttons/Button.vue';
-import Input from '~/components/forms/Input.vue';
+import { cn } from '@/lib/utils';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useToastStore, useUserStore, useSettingsStore } from '~/store';
+import { Loader2Icon } from 'lucide-vue-next';
 
 const router = useRouter();
 const toastStore = useToastStore();
 const userStore = useUserStore();
+const settingsStore = useSettingsStore();
+const isLoading = ref(false);
 
 // Form models
 const username = ref('');
@@ -76,7 +110,9 @@ const login = async () => {
 		return;
 	}
 
+	isLoading.value = true;
 	await userStore.login(username.value, password.value);
+	isLoading.value = false;
 	if (loggedIn.value) await router.push('/dashboard/uploads');
 };
 </script>
