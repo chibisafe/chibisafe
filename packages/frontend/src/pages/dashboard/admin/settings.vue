85a764dee768884fd4c1f829b6d040623e7909d7
Kana <heyitspitu@gmail.com>
2023-09-16 04:22:59 +0900

    feat: better settings page

--- packages/frontend/src/pages/dashboard/admin/settings.vue
+++ packages/frontend/src/pages/dashboard/admin/settings.vue
@@ -13,99 +13,72 @@
 					}
 				]"
 			/>
-			<h1 class="text-2xl mt-8 mb-8 font-semibold text-light-100 flex items-center mobile:flex-col">
-				Settings
-				<span class="grow h-1 w-full"></span>
+			<h1 class="text-2xl mt-8 mb-8 font-semibold text-light-100 flex items-center mobile:flex-col"></h1>
 
-				<div class="items-center my-8">
-					<button
-						type="button"
-						class="bg-green-700 hover:bg-green-800 text-light-100 font-semibold py-2 px-4 rounded items-center w-64 text-center text-base"
-						@click="saveSettings"
-					>
-						Save
-					</button>
-				</div>
-			</h1>
-			<div class="my-4p-4 bg-dark-110 text-light-100 text-lg p-4">
-				<div v-for="(setting, index) in settings" :key="index" class="flex mb-4 items-center bg-dark-90 p-4">
-					<div v-if="setting.type === 'string' || setting.type === 'number'" class="w-full">
-						<Input
+			<div class="mt-8 my-4 bg-dark-100 text-light-100 text-lg p-4 space-y-4 max-w-3xl m-auto">
+				<div v-for="(setting, index) in settings" :key="index" class="grid gap-2">
+					<template v-if="setting.type === 'string' || setting.type === 'number'">
+						<InputWithLabel
 							v-model="setting.value"
 							:label="setting.name"
-							:type="setting.type === 'string' ? 'text' : 'number'"
-							:notice="setting.notice"
-							:blur="setting.key === 'secret'"
-							class="w-full"
+							:name="setting.key"
+							:type="setting.type"
 						/>
-					</div>
-
-					<div v-else-if="setting.type === 'boolean'" class="w-full">
-						<div class="w-60 text-sm">{{ setting.name }}</div>
-						<Switch
-							v-model="setting.value"
-							class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-0 border-gray-600"
-							:class="[setting.value ? 'bg-blue-400' : 'bg-gray-200']"
-							@update:modelValue="(value: any) => (setting.value = value)"
-						>
-							<span class="sr-only">{{ setting.name }}</span>
-							<span
-								aria-hidden="true"
-								class="pointer-events-none inline-block h-5 w-5 transform rounded-full shadow ring-0 transition duration-200 ease-in-out bg-dark-90"
-								:class="[setting.value ? 'translate-x-5' : 'translate-x-0']"
+					</template>
+					<template v-if="setting.type === 'boolean'">
+						<div class="flex items-center space-x-2 mt-4">
+							<Switch
+								:id="setting.key"
+								:checked="setting.value"
+								@click="setting.value = !setting.value"
 							/>
-						</Switch>
-					</div>
-					<div v-else-if="setting.type === 'object'" class="w-full">
-						<div class="w-60 text-sm">{{ setting.name }}</div>
+							<Label :for="setting.key">{{ setting.name }}</Label>
+						</div>
+					</template>
+
+					<template v-else-if="setting.type === 'object'">
 						<template v-if="setting.key === 'blockedExtensions'">
-							<div class="mt-1 mb-2">
-								<!-- @ts-ignore -->
-								<!-- eslint-disable-next-line vue/component-name-in-template-casing -->
-								<input
+							<div class="grid w-full max-w-sm items-center gap-1.5">
+								<Label :for="setting.key">{{ setting.name }}</Label>
+								<Input
+									:id="setting.key"
 									ref="blockedExtensionsInput"
-									class="shadow focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-700 rounded-sm text-light-100 bg-dark-110"
 									type="text"
 									placeholder="Type here an extension and then hit ENTER"
 									@keydown.enter="(event: Event) => addExtension(setting, event.target)"
 								/>
 							</div>
-							<span
-								v-for="(extension, idx) in String(setting.value).split(',')"
-								:key="idx"
-								class="inline-flex items-center gap-x-0.5 rounded-md bg-gray-700 px-2 py-1 font-medium text-light-100 ring-1 ring-inset ring-gray-500/10 mr-2 mb-2"
-							>
-								{{ extension }}
-								<button
-									type="button"
-									class="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-gray-800"
-									@click="removeExtension(setting, extension)"
+
+							<div>
+								<Badge
+									v-for="(extension, idx) in String(setting.value).split(',')"
+									:key="idx"
+									class="mr-2"
 								>
-									<span class="sr-only">Remove</span>
-									<svg viewBox="0 0 14 14" class="h-3.5 w-3.5 stroke-white group-hover:stroke-white">
-										<path d="M4 4l6 6m0-6l-6 6" />
-									</svg>
-									<span class="absolute -inset-1" />
-								</button>
-							</span>
+									{{ extension }}
+									<span class="ml-2" @click="removeExtension(setting, extension)">
+										<svg viewBox="0 0 14 14" class="h-3.5 w-3.5 stroke-white">
+											<path d="M4 4l6 6m0-6l-6 6" />
+										</svg>
+									</span>
+								</Badge>
+							</div>
 						</template>
+					</template>
+
+					<p class="text-muted-foreground text-sm">
+						<span class="block">{{ setting.description }}</span>
+						<span v-if="setting.example" class="block italic mt-1 bg-dark-90"
+							>Example: {{ setting.example }}</span
+						>
+					</p>
+					<div v-if="setting.notice" class="text-sm italic text-red-400">
+						{{ setting.notice }}
 					</div>
-					<div class="w-1/2 p-4">
-						<p>{{ setting.description }}</p>
-						<p v-if="setting.example" class="bg-dark-80 p-2 mt-1 text-sm italic">
-							Example: {{ setting.example }}
-						</p>
-					</div>
+					<Separator class="my-4" />
 				</div>
-			</div>
-			<div class="items-center w-full my-8">
-				<button
-					type="button"
-					class="bg-green-700 hover:bg-green-800 text-light-100 font-semibold py-2 px-4 rounded items-center w-64 text-center"
-					@click="saveSettings"
-				>
-					Save
-				</button>
+
+				<Button @click="saveSettings">Save settings</Button>
 			</div>
 		</div>
 	</ScrollArea>
@@ -115,10 +88,16 @@
 import { ref, onMounted } from 'vue';
 import { getAdminSettings, setAdminSettings } from '~/use/api';
 import { useSettingsStore } from '~/store';
-import { Switch } from '@headlessui/vue';
-import Input from '~/components/forms/Input.vue';
-import { ScrollArea } from '@/components/ui/scroll-area';
 import Breadcrumbs from '~/components/breadcrumbs/Breadcrumbs.vue';
+import InputWithLabel from '@/components/input/InputWithLabel.vue';
+
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Switch } from '@/components/ui/switch';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Badge } from '@/components/ui/badge';
 
 const settingsStore = useSettingsStore();
 const settings = ref(null) as any;
