d6eaa6d83f4fd59d9d6f1339fa75194c2cf2b975
Kana <heyitspitu@gmail.com>
2023-09-16 02:22:05 +0900

    feat: tooltip and badge components

---
+++ packages/frontend/src/components/ui/tooltip/TooltipProvider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { TooltipProvider, type TooltipProviderProps } from 'radix-vue';
+
+const props = defineProps<TooltipProviderProps>();
+</script>
+
+<template>
+	<TooltipProvider v-bind="props" :delayDuration="100">
+		<slot />
+	</TooltipProvider>
+</template>
