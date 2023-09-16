76c9c613ed41e4b93d0cff7db9ff8d224a095c9a
Kana <heyitspitu@gmail.com>
2023-09-17 07:02:06 +0900

    fix: types

--- packages/frontend/src/components/ui/alert-dialog/AlertDialogTrigger.vue
+++ packages/frontend/src/components/ui/alert-dialog/AlertDialogTrigger.vue
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
 interface ExtendedWithButton extends AlertDialogTriggerProps {
-	variant?: string;
+	variant?: 'default' | 'link' | 'destructive' | 'outline' | 'secondary' | 'ghost' | null | undefined;
 }
 
 const props = defineProps<ExtendedWithButton>();
