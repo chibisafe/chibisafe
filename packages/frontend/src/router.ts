5f15a91ea58630faac6709760f65899576c65447
Pitu <heyitspitu@gmail.com>
2024-01-05 02:45:21 +0900

    feat: navigation safeguards

--- packages/frontend/src/router.ts
+++ packages/frontend/src/router.ts
@@ -1,7 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from '~/store';
 import routes from '~pages';
 
-export default createRouter({
+const router = createRouter({
 	history: createWebHistory(),
 	routes
 });
+
+router.beforeEach(async (to, _, next) => {
+	const userStore = useUserStore();
+	if (!userStore.user.loggedIn) {
+		await userStore.checkToken();
+	}
+
+	if (to.path.startsWith('/dashboard') && !userStore.user.loggedIn) {
+		next('/login');
+		return;
+	}
+
+	next();
+});
+
+export default router;
