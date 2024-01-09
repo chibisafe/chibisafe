60d3f1594a78c1dce54a1fc4b3a99633c7cc7507
Pitu <heyitspitu@gmail.com>
2024-01-10 07:10:06 +0900

    feat: initial commit

---
+++ packages/next/prettier.config.js
@@ -0,0 +1,28 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	...require('../../.prettierrc.json'),
+	importOrder: [
+		'^(react/(.*)$)|^(react$)',
+		'^(next/(.*)$)|^(next$)',
+		'<THIRD_PARTY_MODULES>',
+		'',
+		'^types$',
+		'^@/types/(.*)$',
+		'^@/config/(.*)$',
+		'^@/lib/(.*)$',
+		'^@/hooks/(.*)$',
+		'^@/components/ui/(.*)$',
+		'^@/components/(.*)$',
+		'^@/styles/(.*)$',
+		'^@/app/(.*)$',
+		'',
+		'^[./]'
+	],
+	importOrderSeparation: false,
+	importOrderSortSpecifiers: true,
+	importOrderBuiltinModulesToTop: true,
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+	importOrderMergeDuplicateImports: true,
+	importOrderCombineTypeAndValueImports: true,
+	plugins: ['@ianvs/prettier-plugin-sort-imports']
+};
