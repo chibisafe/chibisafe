a43a4d24afb99c3513721e53f145a59499fdadc5
Pitu <heyitspitu@gmail.com>
2024-01-11 03:57:00 +0900

    chore: more stuff

--- packages/next/src/config/site.ts
+++ packages/next/src/config/site.ts
@@ -1,17 +1,12 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: 'Next.js',
-	description: 'Beautifully designed components built with Radix UI and Tailwind CSS.',
+	name: 'chibisafe',
+	description: 'Beautiful and blazing fast vault to save all your files in the cloud.',
 	mainNav: [
 		{
 			title: 'Home',
 			href: '/'
 		}
-	],
-	links: {
-		twitter: 'https://twitter.com/shadcn',
-		github: 'https://github.com/shadcn/ui',
-		docs: 'https://ui.shadcn.com'
-	}
+	]
 };
