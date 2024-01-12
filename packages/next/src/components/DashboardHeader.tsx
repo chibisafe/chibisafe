8be62c96d6d4489f393589202d4c90eeea4b3f12
Pitu <heyitspitu@gmail.com>
2024-01-13 07:29:31 +0900

    feat: styling and sonner

--- packages/next/src/components/DashboardHeader.tsx
+++ packages/next/src/components/DashboardHeader.tsx
@@ -8,7 +8,7 @@ export function DashboardHeader({ title, subtitle, children }: DashboardHeaderPr
 	return (
 		<div className="flex items-center justify-between px-2">
 			<div className="grid gap-1">
-				<h1 className="font-heading text-3xl md:text-4xl">{title}</h1>
+				<h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">{title}</h1>
 				{subtitle ? <p className="text-lg text-muted-foreground">{subtitle}</p> : null}
 			</div>
 			{children}
