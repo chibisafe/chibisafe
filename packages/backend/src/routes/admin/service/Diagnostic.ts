f2fe22ffc37f7ba4ec6258c8fd7395c1eaf57147
Pitu <heyitspitu@gmail.com>
2024-01-15 03:31:38 +0900

    feat: add diagnostics button

---
+++ packages/backend/src/routes/admin/service/Diagnostic.ts
@@ -0,0 +1,32 @@
+import type { FastifyReply } from 'fastify';
+import type { RequestWithUser } from '@/structures/interfaces.js';
+import envinfo from 'envinfo';
+import jetpack from 'fs-jetpack';
+import { URL, fileURLToPath } from 'node:url';
+
+export const options = {
+	url: '/admin/service/diagnostic',
+	method: 'get',
+	middlewares: ['apiKey', 'auth', 'admin']
+};
+
+export const run = async (_: RequestWithUser, res: FastifyReply) => {
+	const auditPath = fileURLToPath(new URL('../../../../../../logs/chibisafe-audit.json', import.meta.url));
+	const logsAudit = await jetpack.readAsync(auditPath, 'json');
+	const logsFile = logsAudit.files[logsAudit.files.length - 1];
+	const logs = await jetpack.readAsync(logsFile.name, 'utf8');
+
+	const envInfo = await envinfo.run(
+		{
+			System: ['OS', 'CPU', 'Memory', 'Shell'],
+			Binaries: ['Node', 'Yarn', 'npm'],
+			Virtualization: ['Docker']
+		},
+		{
+			showNotFound: true
+		}
+	);
+
+	const diagnostics = envInfo + '\n\n' + logs;
+	return res.send({ diagnostics });
+};
