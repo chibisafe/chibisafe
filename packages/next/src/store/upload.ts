99a876bd3fe059d385cb71210ecc410b67e3c391
Pitu <heyitspitu@gmail.com>
2024-01-16 00:57:06 +0900

    chore: remove unused

--- packages/next/src/store/upload.ts
+++
@@ -1,9 +0,0 @@
-import { atom, createStore } from 'jotai';
-
-export const uploadStore = createStore();
-
-export const isUploadingAtom = atom(false);
-export const fileListAtom = atom<File[] | []>([]);
-
-uploadStore.set(isUploadingAtom, false);
-uploadStore.set(fileListAtom, []);
