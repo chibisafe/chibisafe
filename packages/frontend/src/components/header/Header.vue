be62e89c18756b07499fcb403872c24084b75f11
Lake Ma Nuts <rakema@duck.com>
2024-01-25 09:53:46 -0800

    Adjust text size for header when on mobile resolution

--- packages/frontend/src/components/header/Header.vue
+++ packages/frontend/src/components/header/Header.vue
@@ -10,21 +10,21 @@
 		<div
 			class="flex space-x-4 items-center justify-center mobile:flex-col-reverse mobile:space-x-0 mobile:space-y-4"
 		>
-			<router-link v-if="!loggedIn" to="/login" class="text-light-100 hover:text-blue-500 text-lg mobile:text-xl"
+			<router-link v-if="!loggedIn" to="/login" class="text-light-100 hover:text-blue-500 text-lg mobile:text-2xl"
 				>Login / Register</router-link
 			>
 
 			<router-link
 				v-else
 				to="/dashboard/uploads"
-				class="text-light-100 hover:text-blue-500 text-lg mobile:text-xl"
+				class="text-light-100 hover:text-blue-500 text-lg mobile:text-2xl"
 				>Dashboard</router-link
 			>
 			<a
 				href="/docs"
 				target="_blank"
 				rel="noreferrer noopener"
-				class="text-light-100 hover:text-blue-500 text-lg mobile:text-xl"
+				class="text-light-100 hover:text-blue-500 text-lg mobile:text-2xl"
 				>Docs</a
 			>
 
