ef6971fd160b64b1f8631ec2e54bfa0ea87f72bc
Pitu <heyitspitu@gmail.com>
2024-02-09 00:41:07 +0900

    feat: create album

--- packages/next/src/components/Album.tsx
+++ packages/next/src/components/Album.tsx
@@ -4,6 +4,8 @@ import type { Album as AlbumType } from '@/types';
 
 import { Button } from '@/components/ui/button';
 
+import { AlbumSettingsDialog } from './dialogs/AlbumSettingsDialog';
+
 export const Album = ({ album }: { readonly album: AlbumType }) => {
 	return (
 		<>
@@ -41,17 +43,23 @@ export const Album = ({ album }: { readonly album: AlbumType }) => {
 						backgroundImage: `url(${album.cover})`
 					}}
 				/>
-				<div className="absolute -bottom-10 left-4 right-4 pointer-events-none select-none group-hover:pointer-events-auto">
+				<div className="absolute bottom-0 left-4 right-4 pointer-events-none select-none group-hover:pointer-events-auto">
 					<div className="mb-2 transform-gpu text-2xl font-bold text-white delay-75 duration-200 group-hover:-translate-y-14 group-hover:delay-75 cursor-pointer">
-						<Link href={`/dashboard/albums/${album.uuid}`}>{album.name}</Link>
+						{album.name}
 					</div>
 					<div className="mb-4 transform-gpu text-lg font-normal text-white delay-0 duration-200 group-hover:-translate-y-14 group-hover:delay-100">
 						{album.count} files
 					</div>
-					<Button className="transform-gpu bg-white opacity-0 transition-all duration-100 group-hover:-translate-y-14 group-hover:opacity-100 group-hover:delay-100">
+				</div>
+				<Link
+					href={`/dashboard/albums/${album.uuid}`}
+					className="absolute inset-0 transition-all cursor-pointer duration-100 group-hover:scale-105 group-hover:duration-150"
+				/>
+				<AlbumSettingsDialog>
+					<Button className="absolute -bottom-10 left-4 right-4 transition-all opacity-0 duration-100 group-hover:-translate-y-14 transform-gpu group-hover:opacity-100 group-hover:delay-100">
 						Settings
 					</Button>
-				</div>
+				</AlbumSettingsDialog>
 			</div>
 		</>
 	);
