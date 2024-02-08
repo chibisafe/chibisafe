db1fb908b11302a57824509078fa1065a741c984
Pitu <heyitspitu@gmail.com>
2024-02-09 00:41:07 +0900

    feat: create album

--- packages/next/src/components/dialogs/CreateAlbumDialog.tsx
+++ packages/next/src/components/dialogs/CreateAlbumDialog.tsx
@@ -1,6 +1,11 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { createAlbum } from '@/actions/CreateAlbum';
+import { MessageType } from '@/types';
 import { Plus } from 'lucide-react';
+import { useFormState } from 'react-dom';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -16,8 +21,22 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 export function CreateAlbumDialog() {
+	const [open, setOpen] = useState(false);
+	const [state, formAction] = useFormState(createAlbum, {
+		message: '',
+		type: MessageType.Uninitialized
+	});
+
+	useEffect(() => {
+		if (state.type === MessageType.Error) toast.error(state.message);
+		else if (state.type === MessageType.Success) {
+			toast.success(state.message);
+			setOpen(false);
+		}
+	}, [state.message, state.type]);
+
 	return (
-		<Dialog>
+		<Dialog open={open} onOpenChange={setOpen}>
 			<DialogTrigger asChild>
 				<Button>
 					<Plus className="mr-2 h-4 w-4" />
@@ -25,21 +44,23 @@ export function CreateAlbumDialog() {
 				</Button>
 			</DialogTrigger>
 			<DialogContent className="sm:max-w-[425px]">
-				<DialogHeader>
-					<DialogTitle>New album</DialogTitle>
-					<DialogDescription>Create a new album</DialogDescription>
-				</DialogHeader>
-				<div className="grid gap-4 py-4">
-					<div className="grid grid-cols-4 items-center gap-4">
-						<Label htmlFor="field" className="text-right">
-							Name
-						</Label>
-						<Input id="field" value="Pedro Duarte" className="col-span-3" />
+				<form action={formAction}>
+					<DialogHeader>
+						<DialogTitle>New album</DialogTitle>
+						<DialogDescription>Create a new album</DialogDescription>
+					</DialogHeader>
+					<div className="grid gap-4 py-4">
+						<div className="grid grid-cols-4 items-center gap-4">
+							<Label htmlFor="album" className="text-right">
+								Name
+							</Label>
+							<Input id="album" name="album" placeholder="New album" className="col-span-3" />
+						</div>
 					</div>
-				</div>
-				<DialogFooter>
-					<Button type="submit">Create</Button>
-				</DialogFooter>
+					<DialogFooter>
+						<Button type="submit">Create</Button>
+					</DialogFooter>
+				</form>
 			</DialogContent>
 		</Dialog>
 	);
