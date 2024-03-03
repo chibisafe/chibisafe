d8457ff875d1ba0741b4949b6b305c65d3e4a659
Pitu <heyitspitu@gmail.com>
2024-03-04 00:49:04 +0900

    chore: more cleanup

--- packages/next/src/components/ui/alert-dialog.tsx
+++ packages/next/src/components/ui/alert-dialog.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
 
 import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/styles/button';
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
