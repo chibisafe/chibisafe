a642fe74fae47a173fc9a606b34d0b423c324973
Pitu <heyitspitu@gmail.com>
2024-03-05 22:37:01 +0900

    feat: invites table

--- packages/next/src/app/dashboard/admin/invites/page.tsx
+++ packages/next/src/app/dashboard/admin/invites/page.tsx
@@ -1,22 +1,41 @@
 import type { Metadata } from 'next';
-import { Plus } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
 import { DashboardHeader } from '@/components/DashboardHeader';
-
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import request from '@/lib/request';
+import { InvitesTable } from '@/components/tables/invites-table/InvitesTable';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 export const metadata: Metadata = {
 	title: 'Dashboard - Admin - Invites'
 };
 
 export default async function DashboardPage() {
+	const cookiesStore = cookies();
+	const token = cookiesStore.get('token')?.value;
+	if (!token) redirect('/');
+
+	const authorization = {
+		authorization: `Bearer ${token}`
+	};
+
+	const response = await request.get(`admin/invites`, {}, authorization, {
+		next: {
+			tags: ['invites']
+		}
+	});
 	return (
 		<>
 			<DashboardHeader title="Invites" subtitle="Manage and create new invites">
 				<Button>
 					<Plus className="mr-2 h-4 w-4" />
-					New invite
+					Create new invite
 				</Button>
 			</DashboardHeader>
+			<div className="px-2">
+				<InvitesTable data={response.invites} />
+			</div>
 		</>
 	);
 }
