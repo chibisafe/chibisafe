ffcd65baae297b8e15d7791a472217df82dd83e4
Pitu <heyitspitu@gmail.com>
2024-03-06 00:54:56 +0900

    feat: invite page

--- packages/next/src/components/NavigationUser.tsx
+++ packages/next/src/components/NavigationUser.tsx
@@ -10,7 +10,6 @@ import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	// DropdownMenuSeparator,
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
 import { buttonVariants } from '@/styles/button';
@@ -20,24 +19,10 @@ export function NavigationUser() {
 
 	return currentUser?.uuid ? (
 		<DropdownMenu>
-			<DropdownMenuTrigger className="ml-4">{currentUser.username}</DropdownMenuTrigger>
+			<DropdownMenuTrigger className="ml-4 text-lg font-medium sm:text-sm">
+				{currentUser.username}
+			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
-				{/* <div className="flex items-center justify-start gap-2 p-2">
-					<div className="flex flex-col space-y-1 leading-none">
-						<p className="font-medium">{user?.username}</p>
-					</div>
-				</div>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem asChild>
-					<Link href="/dashboard">Dashboard</Link>
-				</DropdownMenuItem>
-				<DropdownMenuItem asChild>
-					<Link href="/dashboard/billing">Billing</Link>
-				</DropdownMenuItem>
-				<DropdownMenuItem asChild>
-					<Link href="/dashboard/settings">Settings</Link>
-				</DropdownMenuItem>
-				<DropdownMenuSeparator /> */}
 				<DropdownMenuItem
 					className="cursor-pointer"
 					onSelect={async event => {
@@ -53,7 +38,10 @@ export function NavigationUser() {
 	) : (
 		<Link
 			href="/login"
-			className={cn(buttonVariants({ variant: 'secondary', size: 'sm' }), 'px-4 ml-4 items-center')}
+			className={cn(
+				buttonVariants({ variant: 'secondary', size: 'sm' }),
+				'px-4 ml-4 items-center text-lg font-medium sm:text-sm'
+			)}
 		>
 			Login
 		</Link>
