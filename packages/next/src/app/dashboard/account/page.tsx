52bc28a79e45dfb0e2b391485d58b87f9b2b4ec0
Pitu <heyitspitu@gmail.com>
2024-03-10 02:54:21 +0900

    feat: change password

--- packages/next/src/app/dashboard/account/page.tsx
+++ packages/next/src/app/dashboard/account/page.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
 
 import { DashboardHeader } from '@/components/DashboardHeader';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { ChangePassword } from '@/components/ChangePassword';
 
 export const metadata: Metadata = {
 	title: 'Dashboard - Credentials'
@@ -14,6 +18,23 @@ export default async function DashboardPage() {
 				subtitle="Manage your credentials"
 				breadcrumbs={[{ name: 'Credentials', url: '/dashboard/account' }]}
 			/>
+			<div className="px-2">
+				<div className="grid grid-cols-[300px,1fr] gap-4">
+					<div className="grid gap-2">
+						<div className="grid gap-1">
+							<Label htmlFor="currentpassword">Your username</Label>
+							<Input name="username" id="username" readOnly />
+							<p className="text-[0.8rem] text-muted-foreground">
+								Can't be changed, only for display purposes
+							</p>
+						</div>
+
+						<Separator className="my-4" />
+
+						<ChangePassword />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
--- packages/next/src/components/UserProvider.tsx
+++ packages/next/src/components/UserProvider.tsx
@@ -1,23 +1,28 @@
+/* eslint-disable promise/prefer-await-to-then */
+/* eslint-disable promise/prefer-await-to-callbacks */
 'use client';
 
 import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 
-import { getMe } from '@/lib/api';
 import { currentUserAtom } from '@/lib/atoms/currentUser';
+import request from '@/lib/request';
+import { logout } from '@/lib/logout';
 
 export function UserProvider({ shouldFetch = false }: { readonly shouldFetch?: boolean }) {
 	const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
 
 	useEffect(() => {
 		if (!currentUser && shouldFetch) {
-			getMe()
-				// eslint-disable-next-line promise/prefer-await-to-then
+			request
+				.get('user/me')
 				.then(response => {
 					setCurrentUser(response.user);
 				})
-				// eslint-disable-next-line promise/prefer-await-to-then
-				.catch(() => {});
+				.catch((error: any) => {
+					// TODO: If error 401 we need to log out
+					logout();
+				});
 		}
 	}, [currentUser, setCurrentUser, shouldFetch]);
 
