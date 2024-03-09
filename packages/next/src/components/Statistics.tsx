a95b16bca4a905687eeabd3e25e89b2646ee8115
Pitu <heyitspitu@gmail.com>
2024-03-10 06:01:12 +0900

    feat: statistics

---
+++ packages/next/src/components/Statistics.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export { AreaChart } from '@tremor/react';
+export { CategoryBar } from '@tremor/react';
+export { ProgressCircle } from '@tremor/react';
+export { DonutChart, Legend } from '@tremor/react';
