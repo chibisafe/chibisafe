d19356410cc2c878bfe71a3548ff29e7716f8e1d
Pitu <heyitspitu@gmail.com>
2024-03-11 22:57:02 +0900

    feat: show empty masonry when there are no files

--- packages/next/src/components/FilesList.tsx
+++ packages/next/src/components/FilesList.tsx
@@ -4,8 +4,16 @@ import type { File, FilePropsType } from '@/types';
 import { Masonry } from '@/components/Masonry';
 import { Pagination } from '@/components/Pagination';
 
-export async function FilesList({ files, count, type }: { readonly count: number; readonly files: File[]; readonly type: FilePropsType }) {
-	return (
+export async function FilesList({
+	files,
+	count,
+	type
+}: {
+	readonly count: number;
+	readonly files: File[];
+	readonly type: FilePropsType;
+}) {
+	return files.length ? (
 		<div className="grid gap-8">
 			<Suspense>
 				<Pagination itemsTotal={count} />
@@ -14,6 +22,13 @@ export async function FilesList({ files, count, type }: { readonly count: number
 			<Suspense>
 				<Pagination itemsTotal={count} />
 			</Suspense>
+		</div>
+	) : (
+		<div className="flex flex-1 justify-center items-center text-white">
+			<h1 className="border-r border-white/30 inline-block mr-5 pr-6 font-medium text-2xl align-top">:(</h1>
+			<div className="inline-block">
+				<h2 className="text-sm font-normal m-0">There are no files to display yet</h2>
+			</div>
 		</div>
 	);
 }
