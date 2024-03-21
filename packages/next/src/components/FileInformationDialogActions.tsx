b0dd5d1bf467491d4ee065b2d40041277886101a
Pitu <heyitspitu@gmail.com>
2024-03-21 20:41:23 +0900

    feat: show unquarantine menu option

--- packages/next/src/components/FileInformationDialogActions.tsx
+++ packages/next/src/components/FileInformationDialogActions.tsx
@@ -49,7 +49,7 @@ export function FileInformationDialogActions({
 						</DropdownMenuItem>
 					)}
 				</DropdownMenuGroup>
-				{type === 'admin' ? (
+				{type === 'admin' || type === 'quarantine' ? (
 					<>
 						<DropdownMenuSeparator />
 						<DropdownMenuGroup>
