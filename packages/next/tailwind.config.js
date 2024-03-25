56d3eeef3e3a0688711fa5c294f6ff2438b6aedd
iCrawl <buechler.noel@outlook.com>
2024-03-26 00:38:34 +0100

    chore: deps

--- packages/next/tailwind.config.js
+++ packages/next/tailwind.config.js
@@ -183,5 +183,5 @@ module.exports = {
 				/^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/
 		}
 	],
-	plugins: [require('tailwindcss-animate')]
+	plugins: [require('tailwindcss-animate'), require('@tailwindcss/container-queries')]
 };
