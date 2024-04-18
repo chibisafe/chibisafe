0c924ff3b4634ebf8bdeb1d2238dbe4fa7bcf2fa
Pitu <heyitspitu@gmail.com>
2024-04-18 10:34:17 +0900

    fix: db relation

---
+++ packages/backend/src/prisma/migrations/20240418013058_add_missing_relation/migration.sql
@@ -0,0 +1,61 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_settings" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "rateLimitWindow" INTEGER NOT NULL,
+    "rateLimitMax" INTEGER NOT NULL,
+    "secret" TEXT NOT NULL,
+    "serviceName" TEXT NOT NULL,
+    "chunkSize" TEXT NOT NULL,
+    "chunkedUploadsTimeout" INTEGER NOT NULL,
+    "maxSize" TEXT NOT NULL,
+    "generateZips" BOOLEAN NOT NULL,
+    "generateOriginalFileNameWithIdentifier" BOOLEAN NOT NULL DEFAULT false,
+    "generatedFilenameLength" INTEGER NOT NULL,
+    "generatedAlbumLength" INTEGER NOT NULL,
+    "generatedLinksLength" INTEGER NOT NULL DEFAULT 8,
+    "blockedExtensions" TEXT NOT NULL,
+    "blockNoExtension" BOOLEAN NOT NULL,
+    "publicMode" BOOLEAN NOT NULL,
+    "userAccounts" BOOLEAN NOT NULL,
+    "disableStatisticsCron" BOOLEAN NOT NULL,
+    "disableUpdateCheck" BOOLEAN NOT NULL DEFAULT false,
+    "backgroundImageURL" TEXT NOT NULL,
+    "logoURL" TEXT NOT NULL,
+    "metaDescription" TEXT NOT NULL,
+    "metaKeywords" TEXT NOT NULL,
+    "metaTwitterHandle" TEXT NOT NULL,
+    "metaDomain" TEXT NOT NULL DEFAULT '',
+    "serveUploadsFrom" TEXT NOT NULL DEFAULT '',
+    "enableMixedCaseFilenames" BOOLEAN NOT NULL DEFAULT true,
+    "usersStorageQuota" TEXT NOT NULL DEFAULT '0',
+    "useNetworkStorage" BOOLEAN NOT NULL DEFAULT false,
+    "useMinimalHomepage" BOOLEAN NOT NULL DEFAULT false,
+    "useUrlShortener" BOOLEAN NOT NULL DEFAULT false,
+    "S3Region" TEXT NOT NULL DEFAULT '',
+    "S3Bucket" TEXT NOT NULL DEFAULT '',
+    "S3AccessKey" TEXT NOT NULL DEFAULT '',
+    "S3SecretKey" TEXT NOT NULL DEFAULT '',
+    "S3Endpoint" TEXT NOT NULL DEFAULT '',
+    "S3PublicUrl" TEXT NOT NULL DEFAULT ''
+);
+INSERT INTO "new_settings" ("S3AccessKey", "S3Bucket", "S3Endpoint", "S3PublicUrl", "S3Region", "S3SecretKey", "backgroundImageURL", "blockNoExtension", "blockedExtensions", "chunkSize", "chunkedUploadsTimeout", "disableStatisticsCron", "disableUpdateCheck", "enableMixedCaseFilenames", "generateOriginalFileNameWithIdentifier", "generateZips", "generatedAlbumLength", "generatedFilenameLength", "generatedLinksLength", "id", "logoURL", "maxSize", "metaDescription", "metaDomain", "metaKeywords", "metaTwitterHandle", "publicMode", "rateLimitMax", "rateLimitWindow", "secret", "serveUploadsFrom", "serviceName", "useMinimalHomepage", "useNetworkStorage", "userAccounts", "usersStorageQuota") SELECT "S3AccessKey", "S3Bucket", "S3Endpoint", "S3PublicUrl", "S3Region", "S3SecretKey", "backgroundImageURL", "blockNoExtension", "blockedExtensions", "chunkSize", "chunkedUploadsTimeout", "disableStatisticsCron", "disableUpdateCheck", "enableMixedCaseFilenames", "generateOriginalFileNameWithIdentifier", "generateZips", "generatedAlbumLength", "generatedFilenameLength", "generatedLinksLength", "id", "logoURL", "maxSize", "metaDescription", "metaDomain", "metaKeywords", "metaTwitterHandle", "publicMode", "rateLimitMax", "rateLimitWindow", "secret", "serveUploadsFrom", "serviceName", "useMinimalHomepage", "useNetworkStorage", "userAccounts", "usersStorageQuota" FROM "settings";
+DROP TABLE "settings";
+ALTER TABLE "new_settings" RENAME TO "settings";
+CREATE TABLE "new_shortenedLinks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uuid" TEXT NOT NULL,
+    "userId" INTEGER,
+    "identifier" TEXT NOT NULL,
+    "destination" TEXT NOT NULL,
+    "views" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "shortenedLinks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_shortenedLinks" ("createdAt", "destination", "id", "identifier", "userId", "uuid", "views") SELECT "createdAt", "destination", "id", "identifier", "userId", "uuid", "views" FROM "shortenedLinks";
+DROP TABLE "shortenedLinks";
+ALTER TABLE "new_shortenedLinks" RENAME TO "shortenedLinks";
+CREATE UNIQUE INDEX "shortenedLinks_uuid_key" ON "shortenedLinks"("uuid");
+CREATE UNIQUE INDEX "shortenedLinks_identifier_key" ON "shortenedLinks"("identifier");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
