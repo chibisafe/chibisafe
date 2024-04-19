e94fd71d66a70ad4bbde949cf6ee02d0420d5f80
Kana <heyitspitu@gmail.com>
2024-04-20 04:06:38 +0900

    feat: metadata cleanup (#613)

--- packages/next/src/app/(home)/guides/page.tsx
+++ packages/next/src/app/(home)/guides/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { getPosts } from '@/lib/mdx';
 
 export const metadata = {
 	title: 'Guides',
@@ -14,6 +15,7 @@ export const metadata = {
 };
 
 export default function GuidesPage() {
+	const posts = getPosts();
 	return (
 		<div className="py-6 lg:py-10">
 			<div className="space-y-4">
@@ -22,45 +24,22 @@ export default function GuidesPage() {
 			</div>
 			<hr className="my-4" />
 			<div className="grid gap-4 md:grid-cols-2 md:gap-6">
-				<article className="group relative rounded-lg border p-6 shadow-md transition-shadow hover:shadow-lg hover:bg-secondary">
-					<div className="flex flex-col justify-between space-y-4">
-						<div className="space-y-2">
-							<h2 className="text-xl font-medium tracking-tight">Running chibisafe with Docker</h2>
-							<p className="text-muted-foreground">
-								Learn to install and run chibisafe in the preferred way
-							</p>
+				{posts.map(post => (
+					<article
+						key={post.slug}
+						className="group relative rounded-lg border p-6 shadow-md transition-shadow hover:shadow-lg hover:bg-secondary"
+					>
+						<div className="flex flex-col justify-between space-y-4">
+							<div className="space-y-2">
+								<h2 className="text-xl font-medium tracking-tight">{post.metadata.title}</h2>
+								<p className="text-muted-foreground">{post.metadata.summary}</p>
+							</div>
 						</div>
-					</div>
-					<Link href="/guides/running-with-docker" className="absolute inset-0">
-						<span className="sr-only">View</span>
-					</Link>
-				</article>
-
-				<article className="group relative rounded-lg border p-6 shadow-md transition-shadow hover:shadow-lg hover:bg-secondary">
-					<div className="flex flex-col justify-between space-y-4">
-						<div className="space-y-2">
-							<h2 className="text-xl font-medium tracking-tight">Running chibisafe manually</h2>
-							<p className="text-muted-foreground">Set up chibisafe manually without Docker</p>
-						</div>
-					</div>
-					<Link href="/guides/running-manually" className="absolute inset-0">
-						<span className="sr-only">View</span>
-					</Link>
-				</article>
-
-				<article className="group relative rounded-lg border p-6 shadow-md transition-shadow hover:shadow-lg hover:bg-secondary">
-					<div className="flex flex-col justify-between space-y-4">
-						<div className="space-y-2">
-							<h2 className="text-xl font-medium tracking-tight">Using network storage</h2>
-							<p className="text-muted-foreground">
-								Learn how to configure an S3-compatible backend to store your uploads
-							</p>
-						</div>
-					</div>
-					<Link href="/guides/using-network-storage" className="absolute inset-0">
-						<span className="sr-only">View</span>
-					</Link>
-				</article>
+						<Link href={`/guides/${post.slug}`} className="absolute inset-0">
+							<span className="sr-only">View</span>
+						</Link>
+					</article>
+				))}
 			</div>
 		</div>
 	);
