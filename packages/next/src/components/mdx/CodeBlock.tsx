603b002dfeb7a99d820aca08c887242e843ac668
Pitu <heyitspitu@gmail.com>
2024-04-20 03:59:45 +0900

    feat: style codeblock a bit better

--- packages/next/src/components/mdx/CodeBlock.tsx
+++ packages/next/src/components/mdx/CodeBlock.tsx
@@ -3,8 +3,6 @@ import * as React from 'react';
 import { highlighter } from '@/lib/shiki';
 
 export async function CodeBlock({ code }: { readonly code: any }) {
-	// const highlightedCode = await highlightCode(code);
-	// check if children has more than 1 line
 	const multiLine = code.children.split('\n').length > 1;
 
 	if (!multiLine) {
@@ -13,13 +11,18 @@ export async function CodeBlock({ code }: { readonly code: any }) {
 		);
 	}
 
+	const lines = code.children.split('\n');
+	if (lines[lines.length - 1] === '') {
+		lines.pop();
+	}
+
 	return (
 		<section
-			className="overflow-x-auto border border-accent rounded-lg py-4 mb-4 mt-6"
+			className="mdx-code overflow-x-auto border border-accent rounded-lg py-4 mb-4 mt-6"
 			// eslint-disable-next-line react/no-danger
 			dangerouslySetInnerHTML={{
 				// __html: highlightedCode
-				__html: highlighter.codeToHtml(code.children, {
+				__html: highlighter.codeToHtml(lines.join('\n'), {
 					lang: code.className?.replace('language-', '') ?? 'plaintext',
 					theme: 'github-dark-dimmed'
 				})
