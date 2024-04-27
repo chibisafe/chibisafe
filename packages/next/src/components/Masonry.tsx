c249cf76f798971af958dec385fd161745f93fcd
Pitu <heyitspitu@gmail.com>
2024-04-27 12:44:18 +0900

    feat: massive refactor and bulk actions

--- packages/next/src/components/Masonry.tsx
+++ packages/next/src/components/Masonry.tsx
@@ -1,67 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import type { File, FilePropsType, FileWithIndex } from '@/types';
-import { useAtomValue, useSetAtom } from 'jotai';
-import { useSearchParams } from 'next/navigation';
-import { isDialogOpenAtom, selectedFileAtom, currentTypeAtom, allFilesAtom } from '@/lib/atoms/fileInformationDialog';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
+import { isDialogOpenAtom, selectedFileAtom } from '@/lib/atoms/fileInformationDialog';
 import { isFileVideo } from '@/lib/file';
 import { cn } from '@/lib/utils';
 import { Masonry as Plock } from '@/components/ui/plock';
-import { useUploadsQuery } from '@/hooks/useUploadsQuery';
-import { FilesTable } from './tables/files-table/FilesTable';
 import { FileThumbnail } from './FileThumbnail';
-import { isMasonryViewAtom } from '@/lib/atoms/settings';
-import { Switch } from './ui/switch';
-import { Label } from './ui/label';
+import { useLongPress } from '@uidotdev/usehooks';
+import { selectedFilesAtom, selectionActiveAtom } from '@/lib/atoms/selectedFiles';
+import { CircleCheckIcon, CircleIcon } from 'lucide-react';
 import { Button } from './ui/button';
-import { Toggle } from './ui/toggle';
 
-export function Masonry({
-	files,
-	total,
+function FileItem({
+	file,
 	type,
-	albumUuid
+	idx,
+	hoveredFiles,
+	setHoveredFiles
 }: {
-	readonly albumUuid?: string | undefined;
-	readonly files?: File[] | undefined;
-	readonly total?: number | undefined;
+	readonly file: FileWithIndex;
+	readonly hoveredFiles: string[];
+	readonly idx: number;
+	setHoveredFiles(files: string[]): void;
 	readonly type: FilePropsType;
 }) {
-	const searchParams = useSearchParams();
-	const isUploads = type === 'uploads';
-	const isAlbumUploads = type === 'album' && Boolean(albumUuid);
-
-	const currentPage = searchParams.get('page') ? Number.parseInt(searchParams.get('page')!, 10) : 1;
-	const perPage = searchParams.get('limit')
-		? Number.parseInt(searchParams.get('limit')!, 10) > 50
-			? 50
-			: Number.parseInt(searchParams.get('limit')!, 10)
-		: 50;
-	const search = searchParams.get('search') ?? '';
-
-	const { data } = useUploadsQuery({ currentPage, perPage, search, type, albumUuid });
-
 	const setModalOpen = useSetAtom(isDialogOpenAtom);
 	const setSelectedFile = useSetAtom(selectedFileAtom);
-	const setAllFilesAtom = useSetAtom(allFilesAtom);
-	const setCurrentType = useSetAtom(currentTypeAtom);
-	const [hoveredFiles, setHoveredFiles] = useState<string[]>([]);
-	const showMasonry = useAtomValue(isMasonryViewAtom);
-	const [filesToUse, setFilesToUse] = useState<FileWithIndex[]>(
-		(files?.length ? files : isUploads || isAlbumUploads ? data?.files ?? [] : []).map((file, index) => ({
-			...file,
-			index
-		}))
-	);
-	const [isBulkSelecting, setIsBulkSelecting] = useState(false);
-	const [selectedFiles, setSelectedFiles] = useState<FileWithIndex[]>([]);
-
-	useEffect(() => {
-		setCurrentType(type);
-		setAllFilesAtom(filesToUse);
-	}, [filesToUse, setAllFilesAtom, setCurrentType, type]);
+	const [selectedFiles, setSelectedFiles] = useAtom(selectedFilesAtom);
+	const isSelectionActive = useAtomValue(selectionActiveAtom);
+	const isSelected = useMemo(() => selectedFiles.includes(file), [selectedFiles, file]);
 
 	const addToHoveredList = useCallback(
 		(file: File) => {
@@ -69,7 +39,7 @@ export function Masonry({
 			if (hoveredFiles.includes(identifierToUse)) return;
 			setHoveredFiles([...hoveredFiles, identifierToUse]);
 		},
-		[hoveredFiles]
+		[hoveredFiles, setHoveredFiles]
 	);
 
 	const removeFromHoveredList = useCallback(
@@ -78,125 +48,159 @@ export function Masonry({
 			if (!hoveredFiles.includes(identifierToUse)) return;
 			setHoveredFiles(hoveredFiles.filter(file => file !== identifierToUse));
 		},
-		[hoveredFiles]
+		[hoveredFiles, setHoveredFiles]
 	);
 
-	const resetBulkFiles = useCallback(() => {
-		setIsBulkSelecting(false);
-		setSelectedFiles([]);
-	}, []);
-
-	const selectAllFiles = useCallback(() => {
-		setSelectedFiles(filesToUse);
-	}, [filesToUse]);
+	const attrs = useLongPress(
+		e => {
+			// console.log('long press', e);
+			if (!isSelected) {
+				setSelectedFiles([...selectedFiles, file]);
+			}
+		},
+		{
+			// onStart: e => {
+			// 	console.log('Press started');
+			// },
+			// onFinish: e => {
+			// 	console.log('Press finished');
+			// },
+			// onCancel: e => {
+			// 	console.log('Press cancelled');
+			// },
+			threshold: 500
+		}
+	);
 
 	return (
-		<div className="flex flex-col w-full h-full gap-4">
-			<div className="h-10 flex flex-row gap-2 items-center">
-				<Toggle
-					variant="outline"
-					pressed={isBulkSelecting}
-					onPressedChange={value => {
-						if (value === false) {
-							resetBulkFiles();
-						}
+		<div
+			className={cn(
+				"relative w-full h-auto transition-all duration-200 hover:duration-150 outline outline-transparent hover:z-40 after:absolute after:-inset-0 after:bg-gradient-to-t after:from-[rgb(4_21_47_/_0.5)] after:via-[rgb(19_36_61_/_0.1)] after:content-[''] after:pointer-events-none group",
+				{
+					'cursor-not-allowed': file.quarantine && type !== 'quarantine'
+				},
+				[
+					isSelectionActive
+						? 'after:via-0% hover:after:via-100%'
+						: 'md:hover:scale-105 md:hover:outline-4 md:hover:outline-[hsl(216_77%_45%)] hover:after:from-transparent hover:after:via-transparent'
+				],
+				{
+					'outline-4 outline-[hsl(46,77%,45%)]': isSelectionActive && isSelected
+				}
+			)}
+			key={idx}
+			onMouseEnter={() => (isFileVideo(file) ? addToHoveredList(file) : null)}
+			onMouseLeave={() => (isFileVideo(file) ? removeFromHoveredList(file) : null)}
+		>
+			<div
+				className={cn(
+					'absolute right-0 top-0 z-20 hidden pointer-events-none group-hover:hidden group-hover:pointer-events-auto md:group-hover:flex',
+					{
+						'pointer-events-auto': isSelectionActive,
+						flex: isSelected
+					}
+				)}
+			>
+				<Button
+					variant="link"
+					onClick={e => {
+						e.preventDefault();
+						e.stopPropagation();
 
-						setIsBulkSelecting(value);
+						if (isSelected) {
+							setSelectedFiles(selectedFiles.filter(f => f !== file));
+						} else {
+							setSelectedFiles([...selectedFiles, file]);
+						}
 					}}
 				>
-					Bulk actions
-				</Toggle>
-
-				{isBulkSelecting ? (
-					<>
-						{/* <span className="text-sm font-medium ">{selectedFiles.length} selected</span> */}
-						<Button onClick={() => selectAllFiles()} variant="outline" className="text-sm font-medium">
-							Select all
-						</Button>
-						{selectedFiles.length ? (
-							<>
-								{/* <Button
-									onClick={() => resetBulkFiles()}
-									variant="outline"
-									className="text-sm font-medium"
-								>
-									Clear
-								</Button> */}
-								<Button size="sm" className="text-sm font-medium">
-									with {selectedFiles.length} selected...
-								</Button>
-							</>
-						) : null}
-					</>
-				) : null}
+					{isSelected ? <CircleCheckIcon className="h-6 w-6" /> : <CircleIcon className="h-6 w-6" />}
+				</Button>
 			</div>
-			{showMasonry ? (
-				<Plock
-					items={filesToUse}
-					config={{
-						columns: [2, 2, 3, 4],
-						gap: [10, 14, 14, 14],
-						media: [640, 1024, 1400, 1400]
-					}}
-					className="px-1"
-					render={(file, idx) => (
-						<div
-							className={cn(
-								"relative w-full h-auto transition-all duration-200 hover:duration-150  outline outline-transparent hover:z-50 after:absolute after:-inset-0 after:bg-gradient-to-t after:from-[rgb(4_21_47_/_0.5)] after:via-[rgb(19_36_61_/_0.1)] after:content-[''] after:pointer-events-none",
-								{
-									'cursor-not-allowed': file.quarantine && type !== 'quarantine'
-								},
-								[
-									isBulkSelecting
-										? 'after:via-0% hover:after:via-100%'
-										: 'hover:scale-105 hover:outline-4 hover:outline-[hsl(216_77%_45%)] hover:after:from-transparent hover:after:via-transparent'
-								],
-								{
-									'outline-4 outline-[hsl(216_77%_45%)]':
-										isBulkSelecting && selectedFiles.includes(file)
-								}
-							)}
-							key={idx}
-							onMouseEnter={() => (isFileVideo(file) ? addToHoveredList(file) : null)}
-							onMouseLeave={() => (isFileVideo(file) ? removeFromHoveredList(file) : null)}
-						>
-							<a
-								className={cn('w-full h-full absolute top-0 left-0 pointer-events-auto', {
-									'pointer-events-none': file.quarantine && type !== 'quarantine'
-								})}
-								href={file.url}
-								target="_blank"
-								rel="noopener noreferrer"
-								draggable={false}
-								onClick={e => {
-									e.preventDefault();
-									e.stopPropagation();
-									if (file.quarantine && type !== 'quarantine') {
-										return;
-									}
+			<button
+				type="button"
+				className={cn('w-full h-full absolute top-0 left-0 pointer-events-auto hidden', {
+					'pointer-events-none': file.quarantine && type !== 'quarantine',
+					block: isSelectionActive
+				})}
+				onClick={() => {
+					if (file.quarantine && type !== 'quarantine') {
+						return;
+					}
 
-									if (isBulkSelecting) {
-										if (selectedFiles.includes(file)) {
-											setSelectedFiles(selectedFiles.filter(f => f !== file));
-										} else {
-											setSelectedFiles([...selectedFiles, file]);
-										}
+					if (isSelectionActive) {
+						if (isSelected) {
+							setSelectedFiles(selectedFiles.filter(f => f !== file));
+						} else {
+							setSelectedFiles([...selectedFiles, file]);
+						}
+					}
+				}}
+			/>
+			<a
+				className={cn('w-full h-full absolute top-0 left-0 pointer-events-auto', {
+					'pointer-events-none': file.quarantine && type !== 'quarantine',
+					hidden: isSelectionActive
+				})}
+				href={file.url}
+				target="_blank"
+				rel="noopener noreferrer"
+				draggable={false}
+				{...attrs}
+				style={{ WebkitTouchCallout: 'none' }}
+				onClick={e => {
+					e.preventDefault();
+					e.stopPropagation();
 
-										return;
-									}
+					if (file.quarantine && type !== 'quarantine') {
+						return;
+					}
 
-									setSelectedFile(file);
-									setModalOpen(true);
-								}}
-							/>
+					setSelectedFile(file);
+					setModalOpen(true);
+				}}
+			/>
 
-							<FileThumbnail file={file} hoveredFiles={hoveredFiles} type={type} />
-						</div>
-					)}
-				/>
-			) : (
-				<FilesTable data={files?.length ? files : data?.files ?? []} type={type} />
-			)}
+			<FileThumbnail file={file} hoveredFiles={hoveredFiles} type={type} />
 		</div>
 	);
+}
+
+export function Masonry({
+	files,
+	type
+}: {
+	readonly files?: FileWithIndex[] | undefined;
+	readonly type: FilePropsType;
+}) {
+	const [hoveredFiles, setHoveredFiles] = useState<string[]>([]);
+	const setSelectedFiles = useSetAtom(selectedFilesAtom);
+
+	useEffect(() => {
+		return () => {
+			setSelectedFiles([]);
+		};
+	}, [setSelectedFiles]);
+
+	return files?.length ? (
+		<Plock
+			items={files}
+			config={{
+				columns: [2, 2, 3, 4],
+				gap: [10, 14, 14, 14],
+				media: [640, 1024, 1400, 1400]
+			}}
+			className="px-1"
+			render={(file, idx) => (
+				<FileItem
+					key={idx}
+					file={file}
+					type={type}
+					idx={idx}
+					hoveredFiles={hoveredFiles}
+					setHoveredFiles={setHoveredFiles}
+				/>
+			)}
+		/>
+	) : null;
 }
