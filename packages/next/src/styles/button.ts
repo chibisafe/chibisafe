3ac02282a826611e6ee032c6826e77704c1bf89d
Pitu <heyitspitu@gmail.com>
2024-05-27 03:04:48 +0900

    chore: add small offset to pressed buttons

--- packages/next/src/styles/button.ts
+++ packages/next/src/styles/button.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-	'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+	'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background transform active:translate-y-[0.0625rem]',
 	{
 		variants: {
 			variant: {
