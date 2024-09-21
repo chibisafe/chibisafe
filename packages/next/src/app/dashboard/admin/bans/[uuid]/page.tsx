d5b27e7fd09a2837ebb1e94142fc3f6cbc63581d
Nemanja Stanić <hai@purr.gg>
2024-09-21 12:53:52 +0300

    fix(bans): readd uuid page

---
+++ packages/next/src/app/dashboard/admin/bans/[uuid]/page.tsx
@@ -0,0 +1,84 @@
+import type { Metadata } from 'next';
+
+import { DashboardHeader } from '@/components/DashboardHeader';
+import type { PageQuery } from '@/types';
+import { Pagination } from '@/components/Pagination';
+import { FilesWrapper } from '@/components/FilesWrapper';
+import { FileDialog } from '@/components/dialogs/FileDialog';
+import { openAPIClient } from '@/lib/serverFetch';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+	title: 'Dashboard - Admin - IPs - IP'
+};
+
+export default async function DashboardPage({
+	params,
+	searchParams
+}: {
+	readonly params: { uuid: string };
+	readonly searchParams: PageQuery;
+}) {
+	const currentPage = searchParams.page ?? 1;
+	const perPage = searchParams.limit ? (searchParams.limit > 50 ? 50 : searchParams.limit) : 50;
+	const search = searchParams.search ?? '';
+
+	const {
+		data: ipData,
+		error,
+		response
+	} = await openAPIClient.GET('/api/v1/ip-bans/{uuid}', {
+		params: {
+			path: {
+				uuid: params.uuid
+			}
+		}
+	});
+
+	if (response.status === 401) {
+		redirect('/login');
+	}
+
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	}
+
+	const { data, error: filesError } = await openAPIClient.GET('/api/v1/ip-bans/{uuid}/files', {
+		params: {
+			path: {
+				uuid: params.uuid
+			},
+			query: {
+				offset: currentPage - 1,
+				limit: perPage,
+				search
+			}
+		}
+	});
+
+	if (filesError) {
+		return <div>Error: {filesError.message}</div>;
+	}
+
+	return (
+		<>
+			<DashboardHeader
+				title={`${ipData.ip} files`}
+				subtitle="As an admin, you can manage their files"
+				breadcrumbs={[
+					{ name: 'Admin', url: '/dashboard/admin' },
+					{ name: 'Banned IPs', url: '/dashboard/admin/ip' },
+					{ name: ipData.ip, url: `/dashboard/admin/ip/${ipData.ip}` }
+				]}
+			/>
+			<div className="px-2 w-full">
+				<div className="grid gap-4">
+					<Pagination itemsTotal={data.count} type="admin" />
+					<FilesWrapper files={data.results} total={data.count} type="admin" />
+					<Pagination itemsTotal={data.count} type="admin" />
+					<FileDialog />
+				</div>
+			</div>
+		</>
+	);
+}
