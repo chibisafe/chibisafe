32e45e7ba3b2b1cec8c53f163e9a11b006050675
Pitu <heyitspitu@gmail.com>
2025-02-01 02:33:20 -0300

    fix: slider

--- packages/next/src/components/CompareSlider.tsx
+++ packages/next/src/components/CompareSlider.tsx
@@ -3,8 +3,11 @@
 
 import { ReactCompareSlider } from 'react-compare-slider';
 import { useEffect, useRef, useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { settingsAtom } from '@/lib/atoms/settings';
 
 export default function CompareSlider({ file1, file2 }: { readonly file1: string; readonly file2: string }) {
+	const settings = useAtomValue(settingsAtom);
 	const imgRef = useRef<HTMLImageElement>(null);
 	const [isVertical, setIsVertical] = useState(false);
 
@@ -18,8 +21,8 @@ export default function CompareSlider({ file1, file2 }: { readonly file1: string
 	return (
 		<ReactCompareSlider
 			className={`w-full ${isVertical ? 'h-auto' : 'h-screen'}`}
-			itemOne={<img src={`${process.env.BASE_API_URL}/${file1}`} alt="Image 1" ref={imgRef} />}
-			itemTwo={<img src={`${process.env.BASE_API_URL}/${file2}`} alt="Image 2" />}
+			itemOne={<img src={`${settings?.serveUploadsFrom}/${file1}`} alt="Image 1" ref={imgRef} />}
+			itemTwo={<img src={`${settings?.serveUploadsFrom}/${file2}`} alt="Image 2" />}
 		/>
 	);
 }
